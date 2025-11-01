6bd9d7fcb0a42001df83f1a2255c0a54b3edf453
Aliaksandr <Paliakou>
2025-11-01 01:15:35 +0100

    Initial Commit

---
+++ 1 задания/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1 задания
+//
+//  Created by Squeze on 01/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
